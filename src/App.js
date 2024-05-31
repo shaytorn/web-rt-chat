6b2516d6756eacaba6c6db185b9110863b05af23
shaytorn <54800093+shaytorn@users.noreply.github.com>
2024-05-31 12:17:00 +0500

    yes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Room from './pages/Room';
 import Main from './pages/Main';
 import NotFound404 from './pages/NotFound404';
@@ -6,11 +6,11 @@ import NotFound404 from './pages/NotFound404';
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
+      <Switch>
         <Route exact path='/room/:id' element={<Room/>}/>
         <Route exact path='/' element={<Main/>}/>
         <Route element={<NotFound404/>}/>
-      </Routes>
+      </Switch>
     </BrowserRouter>
   );
 }
